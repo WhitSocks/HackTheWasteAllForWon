3ae69e815f692d42400194cf3362ecdf3300947b
WhitSocks <87972260+WhitSocks@users.noreply.github.com>
2023-04-01 02:49:16 +0200

    added login script

--- Script.js
+++ Script.js
@@ -1,3 +1,27 @@
+
+function login() {
+    const loginBtn = document.querySelector('#login-btn');
+    
+    loginBtn.addEventListener('click', handleLogin);
+    
+    function handleLogin(event) {
+        event.preventDefault();
+    
+        const email = document.querySelector('#email').value;
+        const password = document.querySelector('#password').value;
+    
+        if (email === 'user@example.com' && password === 'password') {
+            window.location.href = '/dashboard';
+        }
+        else 
+        {
+            alert('Invalid email or password. Please try again.');
+        }
+    }
+    }
+
+
+
 function allowDrop(ev) {
     ev.preventDefault();
    
